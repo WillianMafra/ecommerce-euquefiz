d61f784ee5841bb799d48756db10e3da6e365a5f
Willian Matheus Nunes Mafra <williannunes965@gmail.com>
2022-09-03 14:27:24 -0300

    update home

--- euquefiz/resources/views/home.blade.php
+++ euquefiz/resources/views/home.blade.php
@@ -1,1 +1,6 @@
 <p>Página home</p>
+<a href="/eventos">Eventos</a>
+<a href="/produtos">Produtos</a>
+<a href="/minha-conta">Minha conta</a>
+<a href="/entrar">Login</a>
+<a href="/sobre">Quem somos</a>
