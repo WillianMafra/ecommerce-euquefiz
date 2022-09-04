a5d95fc83fc12ee937bab2ca6f03513b5b0d8eca
Matheus Dos Santos <99matheussan@gmail.com>
2022-09-04 02:39:01 -0300

    Melhoria na Nav para ficar 100% responsivo, criação do footer com contatos, eventos recentes, modificação dos controllers para serem mais coerentes com a nomenclatura, correção da web.php para ficar 100% a modificação dos controllers, criação do arquivo footer.css

--- euquefiz/resources/views/layouts/layout.blade.php
+++ euquefiz/resources/views/layouts/layout.blade.php
@@ -1,45 +1,104 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<div class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="{{asset('css/layout.css')}}">
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>@yield('Eu Que Fiz', 'Eu Que Fiz')</title>
+</head>
+<header>
+    <nav class="navbar navbar-default  navbar-inverse navbar-dark " role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="{{route('home')}}" class="navbar-brand">
+                    <img class="img-thumbnail" id="icone-logo-principal" src="{{asset('storage/img/logo-design/EuQueFizLogo.jpg')}}" width="70" height="30" alt="" />
+                </a>
+            </div>
 
-                <div class="navbar-header">
-                    <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                    <a href="{{route('home')}}" class="navbar-brand">Eu Que Fiz</a>
-                </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{route('products')}}">Produtos</a></li>
+                    <li><a href="{{route('events')}}">Eventos</a></li>
+                    <li><a href="{{route('aboutus')}}">Sobre</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Quem Somos? <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Integrante 1</a></li>
+                            <li><a href="#">Integrante 2</a></li>
+                            <li><a href="#">Integrante 3</a></li>
+                            <li><a href="#">Integrante 4</a></li>
+                            <li><a href="#">Integrante 5</a></li>
+                            <li><a href="#">Integrante 6</a></li>
+                            <li><a href="#">Integrante 7</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#"><img src="{{asset('storage/img/icone/shopping-bag.png')}}" alt="icone-carrinho" width="30" height="25"></a>
+                    </li>
+                    <li><a href="{{route('account')}}"><span class="glyphicon glyphicon-user"></span> Perfil</a></li>
+                    <li><a  class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-log-in"></span> Login / Cadastrar <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{route('login')}}">Login</a></li>
+                            <li><a href="{{route('register')}}">Cadastrar</a></li>
 
-                <div class="navbar-collapse collapse" id="mobile_menu">
-                    <ul class="nav navbar-nav">
-                        <li><a href="{{route('products')}}">Produtos</a></li>
-                        <li><a href="{{route('events')}}">Eventos</a></li>
-
-                        <li><a href="/sobre" class="dropdown-toggle" data-toggle="dropdown">Quem Somos <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">DropDown Um</a></li>
-                                <li><a href="#">DropDown Dois</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="/eventos">Eventos</a></li>
-                    </ul>
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#"><img src="{{asset('storage/img/icone/shopping-bag.png')}}" width="30" height="25"></a>
-                        </li>
-                        <li><a href="{{route('account')}}"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-                        <li><a href="{{route('register')}}" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-log-in"></span> Login / Cadastrar <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="{{route('login')}}">Login</a></li>
-                                <li><a href="{{route('register')}}">Cadastrar</a></li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+
+</header>
+<footer class="footer-espaco text-center">
+    <div class="container-fluid mt-1" style="background-color:#eff5f5;">
+        <div class="row  row-no-gutters">
+            <div class="col-xs-12 col-sm-6 col-md-3" >
+                <ul>
+                    <li class="col-heading">Contatos</li>
+                    <li>
+                        <i class="fa fa-phone" aria-hidden="true"></i><a href="tel:99-999-999-9999">Telefone Para Contato</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i><a href="#">Endereço</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-envelope-square" aria-hidden="true"></i><a href="mailto:someone@yoursite.com?subject=Email Subject line">Nós Envie um E-mail</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3" >
+                <ul>
+                    <li class="col-heading">Subheading</li>
+                    <li><a href="#">Link to page</a></li>
+                    <li><a href="#">Link to page</a></li>
+                    <li><a href="#">Link to page</a></li>
+                    <li><a href="#">Link to page</a></li>
+                </ul>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3" >
+                <div class="col-heading">Eventos Recentes</div>
+                    <a href=""><img class="img-thumbnail" src="{{asset('storage/img/logo-design/EuQueFizLogo.jpg')}}" width="60" alt="" /></a>
+                    <a href=""><img class="img-thumbnail" src="{{asset('storage/img/logo-design/EuQueFizLogo.jpg')}}" width="60" alt="" /></a>
+                    <a href=""><img class="img-thumbnail" src="{{asset('storage/img/logo-design/EuQueFizLogo.jpg')}}" width="60" alt="" /></a>
+                    <a href=""><img class="img-thumbnail" src="{{asset('storage/img/logo-design/EuQueFizLogo.jpg')}}" width="60" alt="" /></a>
+            </div>
+        </div>
+
     </div>
-</div>
+</footer>
+</html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--- euquefiz/routes/web.php
+++ euquefiz/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\OthersController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GeralController;
 use App\Http\Controllers\AdminController;
 
 /*
@@ -14,18 +16,17 @@ use App\Http\Controllers\AdminController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [GeralController::class, 'home'])->name('home');
-
-
 Route::get('/admin', [AdminController::class, 'admin'])->name('admin');
 
-Route::get('/eventos', [GeralController::class, 'events'])->name('events');
-Route::get('/produtos', [GeralController::class, 'products'])->name('products');
+Route::get('/', [ProductsController::class, 'home'])->name('home');
+Route::get('/produtos', [ProductsController::class, 'products'])->name('products');
 
-Route::get('/minha-conta', [GeralController::class, 'account'])->name('account');
-Route::get('/register', [GeralController::class, 'register'])->name('register');
-Route::get('/entrar', [GeralController::class, 'login'])->name('login');
 
-Route::get('/sobre', [GeralController::class, 'aboutus'])->name('aboutus');
+Route::get('/minha-conta', [UserController::class, 'account'])->name('account');
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::get('/entrar', [UserController::class, 'login'])->name('login');
+
+Route::get('/eventos', [OthersController::class, 'events'])->name('events');
+Route::get('/sobre', [OthersController::class, 'aboutus'])->name('aboutus');
 
 
