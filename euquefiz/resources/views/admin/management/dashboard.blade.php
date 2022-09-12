3797ad00b13780ffca6e88e2f078d77b18bfcd83
Matheus Dos Santos <99matheussan@gmail.com>
2022-09-11 22:34:55 -0300

    atualizacao

--- euquefiz/resources/views/admin/management/dashboard.blade.php
+++ euquefiz/resources/views/admin/management/dashboard.blade.php
@@ -1,76 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Administração</title>
-    <meta name="robots" content="noindex, nofollow">
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-    <link href="{{asset('storage/img/logo-design/LogoSemFundo.png')}}" rel="icon">
-    <link href="{{asset('storage/img/logo-design/LogoSemFundo.png')}}" rel="apple-touch-icon">
-
-    <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/admin/dashboard/style.css')}}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{asset('css/admin/dashboard/css.css')}}">
-</head>
-<body class="bg-dark">
-<header id="header" class="header fixed-top d-flex align-items-center bg-dark">
-    <div class="d-flex align-items-center justify-content-between"> <a href="#" class="logo d-flex align-items-center">
-            <img src="{{asset('storage/img/logo-design/LogoSemFundo.png')}}" alt="Eu Que Fiz Logo">
-                <span class="d-none d-lg-block" id="titulo" >Eu Que Fiz</span></a><i class="bi bi-list toggle-sidebar-btn text-primary"></i>
-    </div>
-    <div class="search-bar p-3 ">
-        <form class="search-form d-flex align-items-center" method="POST" action="#"> <input type="text" name="query" placeholder="Pesquisar Produtos" title="Procurar Produtos"> <button type="submit" title="Pesquisar"><i class="bi bi-search"></i></button></form>
-    </div>
-    <nav class="header-nav ms-auto bg-light rounded p-2">
-        <ul class="d-flex align-items-center">
-            <li class="nav-item d-block d-lg-none"><a class="nav-link nav-icon search-bar-toggle " href="#"> <i class="bi bi-search"></i></a></li>
-            <li class="nav-item dropdown">
-                <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown"> <i class="bi bi-bell"></i> <span class="badge bg-primary badge-number">4</span> </a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                    <li class="dropdown-header"> Você tem X notificações <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">Ver Todos</span></a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li class="notification-item">
-                        <i class="bi bi-exclamation-circle text-warning"></i>
-                        <div>
-                            <h4>Titulo</h4>
-                            <p>Descricao</p>
-                            <p>Quanto tempo atrás</p>
-                        </div>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li class="dropdown-footer"> <a href="#">Exibir Todas As Notificações</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown p-3 bg-light rounded">
-                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown"> <img src="{{asset('storage/img/chefs/walter-white.jpg')}}" alt="Profile" class="rounded-circle"> <span class="d-none d-md-block dropdown-toggle ps-2">Jassa</span> </a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                    <li class="dropdown-header">
-                        <h6>Jassa</h6>
-                        <span>Admin + Nome Admin</span>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li> <a class="dropdown-item d-flex align-items-center" href="#"> <i class="bi bi-person"></i> <span>Meu Perfil</span> </a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li> <a class="dropdown-item d-flex align-items-center" href="#"> <i class="bi bi-box-arrow-right"></i> <span>Sair</span> </a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-</header>
+@extends('admin.management.adminHeader')
+@section('adminDashboard')
 <aside id="sidebar" class="sidebar bg-dark">
     <ul class="sidebar-nav " id="sidebar-nav">
         <li class="nav-item"><a class="nav-link " href="#"><i class="bi bi-grid"></i> <span>Dashboard</span></a></li>
@@ -317,13 +246,4 @@
         </div>
     </section>
 </main>
-<footer id="footer" class="footer">
-    <div class="copyright" id="copyright"> &copy; Copyright <strong><span>Eu Que Fiz</span></strong></div>
-</footer>
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-<script src="{{asset('js/admin/dashboard/main.js')}}"></script>
-<script src="{{asset('js/admin/dashboard/scripts.js')}}"></script>
-</body>
-</html>
+@endsection
