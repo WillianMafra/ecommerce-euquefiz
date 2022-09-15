4d6d9c36b3f58cac2e03036338d4302b7355e814
Bruno Hoffmann Schumacher <brunohoffmann.s@gmail.com>
2022-09-14 23:59:09 -0300

    criando pagina sobre

--- euquefiz/resources/views/others/aboutus.blade.php
+++ euquefiz/resources/views/others/aboutus.blade.php
@@ -1,1 +1,32 @@
-Aqui iremos apresentar sobre a empresa e o projeto.
+@extends('layouts.layout')
+@section('events')
+@push('style')
+    <link rel="stylesheet" href="{{asset('css/aboutus/aboutus.css')}}">
+@endpush
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sobre Nós</title>
+    <meta charset="utf-8"/>
+    <style>
+        
+    </style>
+</head>
+<body>
+<section>
+   <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="events-font">Sobre Nós </h1>
+                <p class="events-font">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        It has survived not only five centuries, but also the leap into electronic typesetting, 
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+                        including versions of Lorem Ipsum
+                </p>
+         </div>
+     </div>
+</section>
+</body>
+</html>
