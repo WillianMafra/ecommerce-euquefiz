d67f76631cc8ae66641a2be9ac93fb2ff3bf4213
Matheus Dos Santos <99matheussan@gmail.com>
2022-09-17 16:33:04 -0300

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('user_name');
             $table->string('endereco');
             $table->string('cidade');
             $table->string('cep');
