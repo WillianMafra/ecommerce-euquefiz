89441eba71db8f3d99e14e788f2e2df248b60a97
Thiago Wolter <thiagowolter7@gmail.com>
2022-09-19 14:01:36 -0300

    correcao conflito aboutus

--- resources/views/others/aboutus.blade.php
+++ resources/views/others/aboutus.blade.php
@@ -6,14 +6,13 @@
 <div class="wrapper-about">
     <div class="testimonial">
         <article>
-            <h1 class="h1-about">Sobre Nos</h1>
+            <h1 class="h1-about">Sobre Nós</h1>
             <img class="about-img" src="{{asset('storage/img/logo-design/logosemfundo.png')}}" alt="">
             <blockquote class="blockquote-about">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora delectus est dolores ratione voluptates, odio obcaecati facilis, temporibus inventore, magni fugit asperiores non ipsum quibusdam a illum fuga amet nam!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora delectus est dolores ratione voluptates, odio obcaecati facilis, temporibus inventore, magni fugit asperiores non ipsum quibusdam a illum fuga amet nam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, vitae hic inventore dicta tenetur corrupti eveniet quam amet repellendus omnis quis, rem molestias ratione a accusantium est natus animi ipsa.
             </blockquote>
             <h5>Eu que fiz!</h5>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <p>chefs abaixo??</p>
         </article>
     </div>
 </div>
