7054fcf3e870f629bc8de366a9cb50c66d2e6f64
Imdebora <104090687+imdebora@users.noreply.github.com>
2022-09-28 15:40:10 -0300

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 
 class ProfileController extends Controller
 {
     public function dices()
-    {
-        return view('profile.profile');
+    {   
+        $users = User::get();
+      
+        return view('profile.profile', compact('users'));
     }
 }
