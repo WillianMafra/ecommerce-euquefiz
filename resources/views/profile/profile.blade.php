ffb21ee930d94ebadc5b2e2533778831442d779d
Matheus Dos Santos <99matheussan@gmail.com>
2022-09-29 19:14:24 -0300

    teste deploy

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -6,21 +6,19 @@
     <link rel="stylesheet" href="{{asset('css/profile/profile.css')}}">
 @endpush
 @section('events')
+<<<<<<< HEAD
     <section>
         <div style="border:solid 1px;" class="container p-5 my-5 bg-white w-75">
             <div class="row">
                 <div class="d-flex justify-content-center align-items-center">
                     <div class="col-md-10 border-right">
                         <div class="p-3 py-5">
-
                             <div class="d-flex justify-content-center align-items-center mb-3">
                                 <h4 class="text-right">Configuração de Perfil</h4>
                             </div>
-
                             @if(!empty($message))
                                 <div class="text-white alert alert-success bg-success text-center">{{$message}}</div>
                             @endif
-
                             <form action="{{route('saveProfile')}}" method="post">
                                 @csrf
                                 @method('PUT')
@@ -115,6 +113,44 @@
                                 </div>
                             </form>
                         </div>
+=======
+<div style="border:solid 1px;width: 600px" class="container p-5 my-5 bg-white">
+    <div class="row">
+    <div class="d-flex justify-content-center">
+        <div class="col-md-3 border-right">
+            <div class="d-flex flex-column align-items-center rounded-circle mt-5">
+                    <label class="picture" for="picture__input" tabIndex="0">
+                        <input type="file" name="picture__input" id="picture__input">
+                        <span class="picture__image"></span>
+                    </label>
+            </div>
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                <span class="font-weight-bold">Nome</span>
+                <span class="text-black-50">Telefone</span>
+                <span class="text-black-50">Cidade</span>
+                <span class="text-black-50">Endereço</span>
+                <span class="text-black-50">CEP</span>
+                <span class="text-black-50">E-mail</span>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="col-md-10 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-center align-items-center mb-3">
+                    <h4 class="text-right">Configuração de Perfil</h4>
+                </div>
+                <div class="d-flex justify-content-center ">
+                    <div class="row mt-2">
+                        <div class="col-md-12"><label class="labels">Nome</label><input type="text" class="form-control" placeholder="Nome completo" value=""></div>
+                        <div class="col-md-12"><label class="labels">Telefone</label><input type="text" class="form-control" placeholder="Digite seu novo número de telefone" value=""></div>
+                        <div class="col-md-12"><label class="labels">Cidade</label><input type="text" class="form-control" placeholder="Digite sua nova cidade" value=""></div>
+                        <div class="col-md-12"><label class="labels">Endereço</label><input type="text" class="form-control" placeholder="Digite seu novo endereço" value=""></div>
+                        <div class="col-md-12"><label class="labels">CEP</label><input type="text" class="form-control" placeholder="Digite seu novo CEP" value=""></div>
+                        <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder=" Digite seu novo e-mail" value=""></div>
+                        <div class="col-md-12"><label class="labels">Nova senha</label><input type="password" class="form-control" name="password" required></div>
+                        <div class="col-md-12"><label class="labels">Confirmar senha</label><input type="password" class="form-control" name="password_confirmation" required></div>
+>>>>>>> 832b1ac8e713b1a181a769bf04c332fc996fd642
                     </div>
                 </div>
             </div>
