2f5d6a098cf515c8336f90dd3a6d50d713a9f30d
Imdebora <104090687+imdebora@users.noreply.github.com>
2022-10-03 15:25:58 -0300

    profile

--- resources/views/account/data.blade.php
+++ resources/views/account/data.blade.php
@@ -5,6 +5,7 @@
 @push('style')
     <link rel="stylesheet" href="{{asset('css/profile/profile.css')}}">
 @endpush
+@section('events')
     <section class="container">
         <div style="border:solid 1px;" class="container p-5 my-5 bg-white w-75">
             <div class="row">
@@ -64,4 +65,5 @@
             </div>
         </div>
     </section>
+@endsection
 
