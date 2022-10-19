9e1c39be97f38630f1d122956d2b2e2fe48113dd
Marli Constantino Meza <104389050+marlicmeza@users.noreply.github.com>
2022-10-19 17:00:18 -0300

    Alteração button pesquisar

--- resources/views/products/productsList.blade.php
+++ resources/views/products/productsList.blade.php
@@ -13,7 +13,7 @@
                 <section class="flex justify-center">
                     <label for="search" class="leading-7 text-sm text-gray-600 ml-2"></label>
                     <input type="text" id="search" name="search" class="placeholder:text-center w-2/4 mx-2.5 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Pesquisar Produto">
-                    <button type="submit" class="inline-flex rounded border border-gray-300  py-2 px-6 focus:outline-none rounded">Pesquisar</button>
+                    <button type="submit" class="inline-flex rounded border border-gray-300  py-1 px-3 focus:outline-none rounded">Pesquisar</button>
                 </section>
             </form>
         </div>
